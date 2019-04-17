51ac7587d9b40ca5e7bcc9e0bf35b35d1ab79e15
Yifan Wang <yifan.wang@moac.io>
2019-04-17 12:38:10 -0700

    bls

---
+++ contracts/add_new_nodes.js
@@ -0,0 +1,127 @@
+const dcbase = require("./deploy_contracts_base.js");
+install_account = dcbase.install_account;
+vnodeLink = dcbase.vnodeLink;
+vnodeRpc = dcbase.vnodeRpc;
+version = dcbase.version;
+password = dcbase.password;
+bmin = dcbase.bmin;
+unlock_forever = dcbase.unlock_forever;
+chain3 = dcbase.chain3;
+check_mark = dcbase.check_mark;
+RED = dcbase.RED;
+GREEN = dcbase.GREEN;
+NC = dcbase.NC;
+green_check_mark = dcbase.green_check_mark;
+subChainProtocolBase = dcbase.subChainProtocolBase;
+vnodeProtocolBase = dcbase.vnodeProtocolBase;
+subChainBase = dcbase.subChainBase;
+minMember = dcbase.minMember;
+maxMember = dcbase.maxMember;
+thousandth = dcbase.thousandth;
+threshold = dcbase.threshold;
+flushRound = dcbase.flushRound;
+tokensupply = dcbase.tokensupply;
+exchangerate = dcbase.exchangerate;
+addFundAmount = dcbase.addFundAmount;
+scs_amount = dcbase.scs_amount;
+scsids = dcbase.scsids;
+scsmonitorids = dcbase.scsmonitorids;
+sendMCPromise = dcbase.sendMCPromise;
+registerSCSSubChainProtocolBasePromise = dcbase.registerSCSSubChainProtocolBasePromise;
+
+chain3.setProvider(new chain3.providers.HttpProvider('http://localhost:52159'));
+chain3.personal.unlockAccount(install_account, password, unlock_forever);
+vnodeProtocolBaseContract = chain3.mc.contract(JSON.parse(vnodeProtocolBaseAbi));
+subChainProtocolBaseContract = chain3.mc.contract(JSON.parse(subChainProtocolBaseAbi));
+subChainBaseContract = chain3.mc.contract(JSON.parse(subChainBaseAbi));
+dappBaseContract = chain3.mc.contract(JSON.parse(dappBaseAbi));
+
+subchainbaseaddr = "0xd6874f1d76130ea6dce3d37f97d33a9022ddd94d";
+subChainBase = chain3.mc.contract(JSON.parse(subChainBaseAbi)).at(subchainbaseaddr);
+subchainprotocolbaseaddr = "0x67013bce15a69ca00a64b3c5e74fb052907c786b";
+subChainProtocolBase = chain3.mc.contract(JSON.parse(subChainProtocolBaseAbi)).at(subchainprotocolbaseaddr);
+
+async function main() {
+    scsids = ["30601cba96b98f22d5c46bb8a8b0b298b8017ef2"]; //scs6
+
+    // send money
+    for (i = 0; i < scsids.length; i++) {
+        scsid = scsids[i];
+        result = await sendMCPromise(chain3, install_account, scsid, scs_amount);
+        console.log("Sent " + scs_amount + " mc to scsid " + scsid + " " + green_check_mark);
+    }
+
+    // register with subchain protocol pool
+    for (i = 0; i < scsids.length; i++) {
+        scsid = scsids[i];
+        result = await registerSCSSubChainProtocolBasePromise(chain3, subChainProtocolBase, scsid);
+    }
+
+    // wait for scs to perform
+    performingSCS = [];
+    while(true) {
+        allPerforming = true;
+        for (i = 0; i < scsids.length; i++) {
+            scsid = scsids[i];
+            //console.log(subChainProtocolBase.scsList("0x" + scsid));
+            if (subChainProtocolBase.isPerforming("0x" + scsid)) {
+                if (performingSCS.indexOf(scsid) == -1) {
+                    console.log("Registered scs: " + scsid + " with pool " + green_check_mark);
+                    performingSCS.push(scsid);
+                }
+            } else {
+                allPerforming = false;
+            }
+        }
+
+        if (allPerforming) {
+            break;
+        }
+    }
+
+    result = await getSCSRolePromise("0x"+ scsids[0]);
+    console.log("get scs role promise for ", scsids[0], "with result ", result);
+
+    nodetoadd = 10;
+    result = await registerAddPromise(nodetoadd);
+    console.log("Register add result with " + result + " " +green_check_mark);
+
+}
+
+// call registeradd to subchainbase
+function registerAddPromise(nodetoadd) {
+    return new Promise((resolve, reject) => {
+        registerAddTransaction = {
+            from: install_account,
+		    to: subChainBase.address,
+		    gas: "1000000",
+		    data: subChainBase.registerAdd.getData(nodetoadd)
+        };
+        chain3.mc.sendTransaction(registerAddTransaction, (e, transactionHash) => {
+            if (!e) {
+                resolve(transactionHash);
+            } else {
+                reject(e);
+            }
+        });
+    });
+}
+
+// call getSCSRole
+function getSCSRolePromise(scsid) {
+    return new Promise((resolve, reject) => {
+        registerAddTransaction = {
+		    to: subChainBase.address,
+		    data: subChainBase.getSCSRole.getData(scsid)
+        };
+        chain3.mc.call(registerAddTransaction, (e, result) => {
+            if (!e) {
+                resolve(result);
+            } else {
+                reject(e);
+            }
+        });
+    });
+}
+
+main();
--- contracts/deploy_contracts.js
+++ contracts/deploy_contracts.js
@@ -1,103 +1,31 @@
-const fs = require("fs");
-const Chain3 = require("chain3");
-const solc = require("solc");
-
-// global setting
-let install_account = "0xa35add395b804c3faacf7c7829638e42ffa1d051";
-let vnodeLink = "vnode:50062";
-let vnodeRpc = "vnode:8545";
-//let version = "1.0.6";
-let version = "dev";
-let password = "123456";
-let bmin = 2;
-let unlock_forever = 0;
-let chain3 = new Chain3();
-let check_mark = "✓";
-let RED = "\033[0;31m";
-let GREEN = "\033[0;32m";
-let NC = "\033[0m";
-let green_check_mark = GREEN + check_mark + NC;
-let subChainProtocolBase = null;
-let vnodeProtocolBase = null;
-let subChainBase = null;
-let minMember = 1;
-let maxMember = 31;
-let thousandth = 1000;
-let threshold = 3;
-let flushRound = 50;
-let tokensupply = 3;
-let exchangerate = 1;
-let addFundAmount = 50;
-
-scs_amount = 1000;
-scsids = [
-    "a63a7764d01a6b11ba628f06b00a1828e5955a7f", // scs 1
-    "43c375d09e8a528770c6e1c76014cc9f4f9139a3", // scs 2
-    "8d26cd8257288a9f3fcb3c7a4b15ade3cf932925", // scs 3
-    "632774bf61ffc8873e43f3ce68cf3f169300efa3", // scs 4
-    "d7e1cf982f75563f166726a5814c7fa3c1948068", // scs 5
-    //"30601cba96b98f22d5c46bb8a8b0b298b8017ef2", // scs 6
-    //"c24c73cfb25e444fb20c3405a8327808303f4040", // scs 7
-];
-
-scsmonitorids = [
-    "0b52dde836cb80a5d13c68784c338f42e1860922"  // monitor
-];
-
-// compile vnodeprotocolbase
-vnodeProtocolBaseSolfile = version + "/" + "VnodeProtocolBase.sol";
-vnodeProtocolBaseContract = fs.readFileSync(vnodeProtocolBaseSolfile, 'utf8');
-vnodeProtocolBaseOutput = solc.compile(vnodeProtocolBaseContract, 1);
-vnodeProtocolBaseAbi = vnodeProtocolBaseOutput.contracts[':VnodeProtocolBase'].interface;
-vnodeProtocolBaseBin = vnodeProtocolBaseOutput.contracts[':VnodeProtocolBase'].bytecode;
-console.log("VnodeProtocolBase Contract compiled, size = " + vnodeProtocolBaseBin.length + " " + green_check_mark);
-
-// compile dappbase
-dappBaseFile = version + "/" + "dappbase.sol";
-dappBaseContract = fs.readFileSync(dappBaseFile, 'utf8');
-dappBaseOutput = solc.compile(dappBaseContract, 1);
-dappBaseAbi = dappBaseOutput.contracts[':DappBase'].interface;
-dappBaseBin = dappBaseOutput.contracts[':DappBase'].bytecode;
-console.log("dappBase Contract compiled, size = " + dappBaseBin.length + " " + green_check_mark);
-
-// compile subchainprotocolbase
-subChainProtocolBaseProtocol = "pos";
-subChainProtocolBaseProtocolType = 0;
-subChainProtocolBaseSolfile = version + "/" + "SubChainProtocolBase.sol";
-subChainProtocolBaseContract = fs.readFileSync(subChainProtocolBaseSolfile, 'utf8');
-subChainProtocolBaseOutput = solc.compile(subChainProtocolBaseContract, 1);
-subChainProtocolBaseAbi = subChainProtocolBaseOutput.contracts[':SubChainProtocolBase'].interface;
-subChainProtocolBaseBin = subChainProtocolBaseOutput.contracts[':SubChainProtocolBase'].bytecode;
-console.log("SubChainProtocolBase Contract compiled, size = " + subChainProtocolBaseBin.length + " " + green_check_mark);
-
-subChainBaseFileName = "SubChainBaseRNG.sol";
-subChainBaseSolfiles = [subChainBaseFileName, "SubChainProtocolBase.sol"];
-//subChainBaseSolfiles = ["SubChainBase.sol", "SubChainProtocolBase.sol"];
-subChainBaseInput = {};
-subChainBaseSolfiles.forEach(fileName => {
-    file = fs.readFileSync(version + "/" + fileName, 'utf8');
-    subChainBaseInput[fileName] = file;
-});
-subChainBaseOutput = solc.compile(
-    {
-        sources: subChainBaseInput,
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200
-            }
-        }
-    },
-    1
-);
-
-if (subChainBaseOutput.errors.length > 0) {
-    console.log(subChainBaseOutput.errors);
-}
-subChainBaseAbi = subChainBaseOutput.contracts[subChainBaseFileName + ':SubChainBase'].interface;
-console.log(subChainBaseAbi);
-subChainBaseBin = subChainBaseOutput.contracts[subChainBaseFileName + ':SubChainBase'].bytecode;
-console.log("SubChainBase Contract compiled, size = " + subChainBaseBin.length + " " + green_check_mark);
+const dcbase = require("./deploy_contracts_base.js");
+install_account = dcbase.install_account;
+vnodeLink = dcbase.vnodeLink;
+vnodeRpc = dcbase.vnodeRpc;
+version = dcbase.version;
+password = dcbase.password;
+bmin = dcbase.bmin;
+unlock_forever = dcbase.unlock_forever;
+chain3 = dcbase.chain3;
+check_mark = dcbase.check_mark;
+RED = dcbase.RED;
+GREEN = dcbase.GREEN;
+NC = dcbase.NC;
+green_check_mark = dcbase.green_check_mark;
+subChainProtocolBase = dcbase.subChainProtocolBase;
+vnodeProtocolBase = dcbase.vnodeProtocolBase;
+subChainBase = dcbase.subChainBase;
+minMember = dcbase.minMember;
+maxMember = dcbase.maxMember;
+thousandth = dcbase.thousandth;
+threshold = dcbase.threshold;
+flushRound = dcbase.flushRound;
+tokensupply = dcbase.tokensupply;
+exchangerate = dcbase.exchangerate;
+addFundAmount = dcbase.addFundAmount;
+scs_amount = dcbase.scs_amount;
+scsids = dcbase.scsids;
+scsmonitorids = dcbase.scsmonitorids;
 
 chain3.setProvider(new chain3.providers.HttpProvider('http://localhost:52159'));
 chain3.personal.unlockAccount(install_account, password, unlock_forever);
@@ -105,6 +33,9 @@ vnodeProtocolBaseContract = chain3.mc.contract(JSON.parse(vnodeProtocolBaseAbi))
 subChainProtocolBaseContract = chain3.mc.contract(JSON.parse(subChainProtocolBaseAbi));
 subChainBaseContract = chain3.mc.contract(JSON.parse(subChainBaseAbi));
 dappBaseContract = chain3.mc.contract(JSON.parse(dappBaseAbi));
+
+sendMCPromise = dcbase.sendMCPromise;
+registerSCSSubChainProtocolBasePromise = dcbase.registerSCSSubChainProtocolBasePromise;
 
 async function main() {
     // deploy two contracts: vnodeprotocolbase, subchainprotocolbase
@@ -117,7 +48,7 @@ async function main() {
     allscsids = scsids.concat(scsmonitorids);
     for (i = 0; i < allscsids.length; i++) {
         scsid = allscsids[i];
-        await sendMCPromise(install_account, scsid, scs_amount);
+        await sendMCPromise(chain3, install_account, scsid, scs_amount);
         console.log("Sent " + scs_amount + " mc to scsid " + scsid + " " + green_check_mark);
     }
 
@@ -134,7 +65,7 @@ async function main() {
     // scs should register with subchainprotocol pool
     for (i = 0; i < scsids.length; i++) {
         scsid = scsids[i];
-        await registerSCSSubChainProtocolBasePromise(scsid);
+        await registerSCSSubChainProtocolBasePromise(chain3, subChainProtocolBase, scsid);
     }
 
     // wait for scs to perform
@@ -212,8 +143,13 @@ async function main() {
         }
     }
 
-    dappBaseContract = await deployDappBaseContractPromise(tokensupply/exchangerate);
+    nonce = 0;
+    dappBaseContract = await deployDappBaseContractPromise(tokensupply/exchangerate, nonce);
     console.log("DappBase Contract deployed! address: "+ dappBaseContract.address + " " + green_check_mark);
+
+    nonce += 1;
+    dappContract = await deployDappContractPromise(0, nonce);
+    console.log("Dapp Contract deployed! address: "+ dappContract.address + " " + green_check_mark);
 
     /*
     // wait for 6 blocks before query for rng node count
@@ -255,25 +191,6 @@ async function main() {
 
 main();
 
-// For add fund to scsids
-function sendMCPromise(src, dest, amount_in_mc) {
-    return new Promise((resolve, reject) => {
-        transaction = {
-            from: src,
-		    value: chain3.toSha(amount_in_mc,'mc'),
-		    to: dest,
-		    gas: "10000000",
-		    data: ""
-        };
-        chain3.mc.sendTransaction(transaction, (e, transactionHash) => {
-            if (!e) {
-                resolve(transactionHash);
-            } else {
-                reject(e);
-            }
-        });
-    });
-}
 
 // For deploy vnodeprotocolbase
 function deployVnodeProtocolBaseContractPromise() {
@@ -326,7 +243,7 @@ function deploySubChainProtocolBaseContractPromise(){
 }
 
 // For deploy dappbase
-function deployDappBaseContractPromise(amount_in_mc){
+function deployDappBaseContractPromise(amount_in_mc, nonce){
     return new Promise((resolve, reject) => {
         deployTransaction = {
             from: install_account,
@@ -336,7 +253,31 @@ function deployDappBaseContractPromise(amount_in_mc){
             gas: "0",
             shardingFlag: "0x3",
             via: install_account,
-            nonce: 0
+            nonce: nonce
+        };
+
+        chain3.mc.sendTransaction(deployTransaction, (e, transactionHash) => {
+            if (!e) {
+                resolve(transactionHash);
+            } else {
+                reject(e);
+            }
+        });
+    });
+}
+
+// For deploy dapp
+function deployDappContractPromise(amount_in_mc, nonce){
+    return new Promise((resolve, reject) => {
+        deployTransaction = {
+            from: install_account,
+            value: chain3.toSha(amount_in_mc,'mc'),
+            to: subChainBase.address,
+            data: '0x' + dappBin,
+            gas: "0",
+            shardingFlag: "0x3",
+            via: install_account,
+            nonce: nonce
         };
 
         chain3.mc.sendTransaction(deployTransaction, (e, transactionHash) => {
@@ -533,14 +474,12 @@ function registerAsVnodeProxy(vnode, via, link, rpclink) {
 function getRNGNodeCountPromise() {
     return new Promise((resolve, reject) => {
         transaction = {
-            from: install_account,
 		    to: subChainBase.address,
-		    gas: "10000000",
 		    data: subChainBase.rngNodeCount.getData()
         };
-        chain3.mc.sendTransaction(transaction, (e, transactionHash) => {
+        chain3.mc.call(transaction, (e, result) => {
             if (!e) {
-                resolve(transactionHash);
+                resolve(result);
             } else {
                 reject(e);
             }
