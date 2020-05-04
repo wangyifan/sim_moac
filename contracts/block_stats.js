9eee201a06de2ba8f013757c7dd512c1bc7e58d9
Yifan Wang <yifan.wang@moac.io>
2020-05-04 12:03:38 -0700

    lottery example

---
+++ contracts/block_stats.js
@@ -0,0 +1,102 @@
+require('console-stamp')(console, 'yyyy-mm-dd HH:MM:ss');
+
+const util = require('util');
+const dcbase = require("./deploy_contracts_base_rng.js");
+install_account = dcbase.install_account;
+vnodeLink = dcbase.vnodeLink;
+vnodeRpc = dcbase.vnodeRpc;
+version = dcbase.version;
+password = dcbase.password;
+bmin = dcbase.bmin;
+unlock_forever = dcbase.unlock_forever;
+chain3 = dcbase.chain3;
+check_mark = dcbase.check_mark;
+RED = dcbase.RED;
+GREEN = dcbase.GREEN;
+NC = dcbase.NC;
+green_check_mark = dcbase.green_check_mark;
+subChainProtocolBase = dcbase.subChainProtocolBase;
+vnodeProtocolBase = dcbase.vnodeProtocolBase;
+subChainBase = dcbase.subChainBase;
+minMember = dcbase.minMember;
+maxMember = dcbase.maxMember;
+thousandth = dcbase.thousandth;
+threshold = dcbase.threshold;
+flushRound = dcbase.flushRound;
+tokensupply = dcbase.tokensupply;
+exchangerate = dcbase.exchangerate;
+addFundAmount = dcbase.addFundAmount;
+scs_amount = dcbase.scs_amount;
+//scsids = dcbase.scsids;
+scsmonitorids = dcbase.scsmonitorids;
+sendMCPromise = dcbase.sendMCPromise;
+registerSCSSubChainProtocolBasePromise = dcbase.registerSCSSubChainProtocolBasePromise;
+getResetRNGGroupPromise = dcbase.getResetRNGGroupPromise;
+
+hostport = "http://"+ "172.20.0.11" + ":" + "8545";
+chain3.setProvider(new chain3.providers.HttpProvider(hostport));
+chain3.personal.unlockAccount(install_account, password, unlock_forever);
+vnodeProtocolBaseContract = chain3.mc.contract(JSON.parse(vnodeProtocolBaseAbi));
+subChainProtocolBaseContract = chain3.mc.contract(JSON.parse(subChainProtocolBaseAbi));
+subChainBaseContract = chain3.mc.contract(JSON.parse(subChainBaseAbi));
+dappBaseContract = chain3.mc.contract(JSON.parse(dappBaseAbi));
+
+subchainbaseaddr = "0x03d60190c33a6b716fef08be1c964182e495f9ff"; // for vss test
+subChainBase = chain3.mc.contract(JSON.parse(subChainBaseAbi)).at(subchainbaseaddr);
+subchainprotocolbaseaddr = "0x67013bce15a69ca00a64b3c5e74fb052907c786b";
+subChainProtocolBase = chain3.mc.contract(JSON.parse(subChainProtocolBaseAbi)).at(subchainprotocolbaseaddr);
+vssbaseaddr = "0xd6874f1d76130ea6dce3d37f97d33a9022ddd94d"; // for vss test
+vssbase = chain3.mc.contract(JSON.parse(vssbaseAbi)).at(vssbaseaddr);
+
+
+async function main() {
+    lastBlockNumber = 0;
+    while(true) {
+        await sleep(1000);
+        try {
+            blockNumber = await getBlockNumber();
+        } catch (e) {
+        }
+
+        if (blockNumber > lastBlockNumber) {
+            for (i = lastBlockNumber + 1; i <= blockNumber; i++) {
+                
+            }
+        }
+    }
+}
+
+// get blocknumber
+function getBlockNumber() {
+    return new Promise((resolve, reject) => {
+        chain3.mc.getBlockNumber((e, blocknumber) => {
+            if (!e) {
+                resolve(blocknumber);
+            } else {
+                //console.log("getBlockNumber reject: " + e);
+                reject(e);
+            }
+        });
+    }).catch("getblocknumber error");
+}
+
+
+function resetVSSGroupPromise(chain3) {
+    return new Promise((resolve, reject) => {
+        resetVSSGroupTransaction = {
+            from: install_account,
+		    to: vssbase.address,
+            gas: "1000000",
+		    data: vssbase.resetVSSGroup.getData()
+        };
+        chain3.mc.sendTransaction(resetVSSGroupTransaction, (e, transactionHash) => {
+            if (!e) {
+                resolve(transactionHash);
+            } else {
+                reject(e);
+            }
+        });
+    });
+}
+
+main();
---
+++ contracts/dev/lottery.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.8;
+
+contract precompileBLS {
+    function f() public returns(bytes32);
+}
+
+contract Lottery {
+  mapping (uint8 => address[]) playersByNumber ;
+  address owner;
+  enum LotteryState { Accepting, Finished }
+  LotteryState state;
+
+  function Lottery() public {
+      owner = msg.sender;
+      state = LotteryState.Accepting;
+  }
+
+  function enter(uint8 number) public payable {
+      require(number<=255);
+      require(state == LotteryState.Accepting);
+      playersByNumber[number].push(msg.sender);
+  }
+
+  function determineWinner() public {
+      require(msg.sender == owner);
+      state = LotteryState.Finished;
+      uint8 winningNumber = random();
+      distributeFunds(winningNumber);
+  }
+
+  function resetLotteryState() public {
+      require(msg.sender == owner);
+      state = LotteryState.Accepting;
+  }
+
+  function distributeFunds(uint8 winningNumber) private returns(uint256) {
+      uint256 winnerCount = playersByNumber[winningNumber].length;
+      uint256 balanceToDistribute = this.balance/(2*winnerCount);
+      for (uint i = 0; i<winnerCount; i++) {
+          playersByNumber[winningNumber][i].transfer(balanceToDistribute);
+      }
+
+      return this.balance;
+  }
+
+  function random() private view returns (uint8) {
+      precompileBLS bls = precompileBLS(0x20);
+      // get the 256-bit random number
+      bytes32 r = bls.f();
+      // return its first 8 bits, range from 0-255
+      return uint8(r[0]);
+  }
+}
